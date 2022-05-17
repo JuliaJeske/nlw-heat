e16120187d0ebd03d63eb34134bcd5fc71821e35
juliajeske <julia.jeske@wehandle.com.br>
2022-05-17 16:51:54 -0300

    finalizacao do backend da aplicacao

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,17 @@
 import {Router } from "express"
 import { AuthenticateUserController } from "./controller/AuthenticateUserControllers"
+import { CreateMessageController } from "./controller/CreateMessageController"
+import { GetLast3MessageController } from "./controller/GetLast3MessagesController"
+import { ProfileUserController } from "./controller/ProfileUserController"
+import { ensureAuthenticate } from "./middleware/ensureAuthenticate"
 
 const router = Router()
 router.post("/authenticate", new AuthenticateUserController().handle)
+
+router.post("/messages",ensureAuthenticate, new CreateMessageController().handle)
+
+router.get("/messages/last3", new GetLast3MessageController().handle)
+router.get("/profile",ensureAuthenticate, new ProfileUserController().handle)
+
 
 export { router }
